e2e99537441ec7647d1b6a415b90473a9931f0ad
Kira Stout <kmstout08@gmail.com>
2023-05-17 22:50:37 -0400

    Stubbed put database tables  with inserts

--- db/welcomedb.sql
+++ db/welcomedb.sql
@@ -5,51 +5,147 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema welcomedb
+-- Schema mydb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `welcomedb` ;
 
 -- -----------------------------------------------------
--- Schema welcomedb
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `welcomedb` DEFAULT CHARACTER SET utf8 ;
-USE `welcomedb` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `user`
+-- Table `mydb`.`category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `user` ;
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT NOT NULL,
-  `username` VARCHAR(80) NOT NULL,
-  `enabled` TINYINT NOT NULL DEFAULT 1,
-  `password` VARCHAR(80) NOT NULL,
-  `created` DATETIME NULL,
-  `updated` DATETIME NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `role` VARCHAR(45) NOT NULL DEFAULT 'standard',
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+CREATE TABLE IF NOT EXISTS `mydb`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
-SET SQL_MODE = '';
-DROP USER IF EXISTS queer1@localhost;
-SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-CREATE USER 'queer1'@'localhost' IDENTIFIED BY 'qweerty';
 
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'queer1'@'localhost';
+-- -----------------------------------------------------
+-- Table `mydb`.`place`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`place` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  `enabled` VARCHAR(45) NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `category_id`),
+  INDEX `fk_place_category2_idx` (`category_id` ASC),
+  CONSTRAINT `fk_place_category2`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `mydb`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`address`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `zip_code` VARCHAR(45) NULL,
+  `place_id` INT NOT NULL,
+  `place_category_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_address_place1_idx` (`place_id` ASC, `place_category_id` ASC),
+  CONSTRAINT `fk_address_place1`
+    FOREIGN KEY (`place_id` , `place_category_id`)
+    REFERENCES `mydb`.`place` (`id` , `category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `role` VARCHAR(45) NULL,
+  `create_date` DATE NULL,
+  `update_date` DATE NULL,
+  `profile_picture` VARCHAR(2500) NULL,
+  `biography` VARCHAR(45) NULL,
+  `enabled` TINYINT NULL,
+  `place_id` INT NULL,
+  `address_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_place1_idx` (`place_id` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_user_place1`
+    FOREIGN KEY (`place_id`)
+    REFERENCES `mydb`.`place` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `mydb`.`address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`message`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`message` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `content` VARCHAR(45) NULL,
+  `create_date` DATETIME NULL,
+  `update_date` VARCHAR(45) NULL,
+  `sender_id` INT NOT NULL,
+  `receiver_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_message_user_idx` (`sender_id` ASC),
+  INDEX `fk_message_user1_idx` (`receiver_id` ASC),
+  CONSTRAINT `fk_message_user`
+    FOREIGN KEY (`sender_id`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_message_user1`
+    FOREIGN KEY (`receiver_id`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`rating`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`rating` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `rating` INT NULL,
+  `comment` VARCHAR(200) NULL,
+  `create_date` DATETIME NULL,
+  `update_date` DATETIME NULL,
+  `place_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_rating_place1_idx` (`place_id` ASC),
+  CONSTRAINT `fk_rating_place1`
+    FOREIGN KEY (`place_id`)
+    REFERENCES `mydb`.`place` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `user`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `welcomedb`;
-INSERT INTO `user` (`id`, `username`, `enabled`, `password`, `created`, `updated`, `email`, `role`) VALUES (1, 'admin', 1, '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', NULL, NULL, 'admin@welcome.com', 'ADMIN');
-
-COMMIT;
-
